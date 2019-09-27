e6c62064c3abb99122e122a1c0a841d891ad3b1a
mwydmuch <marek@wydmuch.poznan.pl>
2019-09-27 10:50:08 +0200

    Update BOP scripts

--- scripts/bop_biology_image_experiments.sh
+++ scripts/bop_biology_image_experiments.sh
@@ -3,24 +3,17 @@
 #bash remap_datasets.sh
 
 DATASETS=(VOC2006 VOC2007 CAL101 CAL256 PROTEIN2)
-ARGS="--threshold 0 -e 0.001 --norm 0"
+ARGS="-e 0.001 --norm 0 --bias 0 --weightsThreshold 0"
 
+#rm -rf models
+#rm -rf outputs
 for d in "${DATASETS[@]}"; do
     echo "${d} tests..."
 
-    bash testBOP.sh "HIERARCHICAL_PREDEFINED_${d}" -m rbop --treeStructure "data/HIERARCHICAL_PREDEFINED_${d}/HIERARCHICAL_PREDEFINED_${d}.hier" ${ARGS}
-    bash testBOP.sh "HIERARCHICAL_RANDOM_${d}" -m rbop --treeStructure "data/HIERARCHICAL_RANDOM_${d}/HIERARCHICAL_RANDOM_${d}.hier" ${ARGS}
-    bash testBOP_with_eps.sh "HIERARCHICAL_PREDEFINED_${d}" -m rbop --treeStructure "data/HIERARCHICAL_PREDEFINED_${d}/HIERARCHICAL_PREDEFINED_${d}.hier" ${ARGS}
-    bash testBOP_with_eps.sh "HIERARCHICAL_RANDOM_${d}" -m rbop --treeStructure "data/HIERARCHICAL_RANDOM_${d}/HIERARCHICAL_RANDOM_${d}.hier" ${ARGS}
+    bash scripts/testBOP.sh "HIERARCHICAL_PREDEFINED_${d}" -m ubopch --treeStructure "data/HIERARCHICAL_PREDEFINED_${d}/HIERARCHICAL_PREDEFINED_${d}.hier" ${ARGS}
+    bash scripts/testBOP.sh "HIERARCHICAL_RANDOM_${d}" -m ubopch --treeStructure "data/HIERARCHICAL_RANDOM_${d}/HIERARCHICAL_RANDOM_${d}.hier" ${ARGS}
 
-    bash testBOP.sh "HIERARCHICAL_PREDEFINED_${d}" -m ubopch --treeStructure "data/HIERARCHICAL_PREDEFINED_${d}/HIERARCHICAL_PREDEFINED_${d}.hier" ${ARGS}
-    bash testBOP.sh "HIERARCHICAL_RANDOM_${d}" -m ubopch --treeStructure "data/HIERARCHICAL_RANDOM_${d}/HIERARCHICAL_RANDOM_${d}.hier" ${ARGS}
+    bash scripts/testBOP.sh "FLAT_${d}" -m ubop ${ARGS}
 
-    bash testBOP.sh "FLAT_${d}" -m ubop ${ARGS}
-
-    # UBOP-CH on flat hidden representation
-    #bash testBOP.sh "FLAT_${d}" -m ubopch --treeType hierarchicalKMeans ${ARGS}
-
-    # UBOP on hierarchical representation
-    #bash testBOP.sh "HIERARCHICAL_PREDEFINED_${d}" -m ubop ${ARGS}
+    bash scripts/testBOP.sh "FLAT_${d}" -m ubopmips ${ARGS}
 done
